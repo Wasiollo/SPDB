92e695e38bb2cafe5ca7460c093e0a71e1afcfdc
Wasiollo <wasiak.mk@gmail.com>
2020-06-15 00:04:37 +0200

    added startPoint endpoint

--- backend/spdb/pathfinder/urls.py
+++ backend/spdb/pathfinder/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import PlacesView, FoodPlacesView, plan_trip
+from .views import PlacesView, FoodPlacesView, plan_trip, StartPointView
 
 urlpatterns = [
     path('places/', PlacesView.as_view(), name="places"),
     path('foodPlaces/', FoodPlacesView.as_view(), name="foodPlaces"),
+    path('startPoint/', StartPointView.as_view(), name="foodPlaces"),
     path('planTrip/', plan_trip, name="planTrip")
+
 ]
